5425c722355c4b3749566b71b1c07b0ee0c1a1f4
Brian Wilson <brianallenwilson@icloud.com>
2021-07-27 10:12:42 -0400

    Initial Commit

---
+++ Shared/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Shared
+//
+//  Created by Brian Wilson on 7/27/21.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
